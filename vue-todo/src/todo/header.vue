2e022d3cdfa5143c806c35284f358e4f9e341be0
jay <keangj@outlook.com>
2018-04-13 00:36:20 +0800

    add vue-todo

---
+++ vue-todo/src/todo/header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="main-header">
+    <h1>ToDo List</h1>
+  </header>
+</template>
+
+<style lang="stylus" scoped>
+  .main-header {
+    text-align center
+    h1 {
+      font-size: 100px
+      color #eee
+      margin 20px
+    }
+  }
+</style>
